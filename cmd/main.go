418d076329b3f3eb35ce748d0434261bbcc3b6d3
Igor Bonfim <igormeirabonfim@gmail.com>
2025-01-25 22:59:16 -0300

    Initial commit

---
+++ cmd/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"products-api/controller"
+	"products-api/db"
+	"products-api/repository"
+	"products-api/usecase"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+
+	dbConnection, err := db.ConnectDB()
+
+	if err != nil {
+		panic(err)
+	}
+
+	ProductRepository := repository.NewProductRepository(dbConnection)
+	ProductUsecase := usecase.NewProductUsecase(ProductRepository)
+	ProductController := controller.NewProductContrtoller(ProductUsecase)
+
+	server.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	server.GET("/products", ProductController.GetProducts)
+	server.GET("products/:productId", ProductController.GetProduct)
+	server.POST("/products", ProductController.CreateProduct)
+
+	server.Run(":8000")
+}
---
+++ controller/product_controller.go
@@ -0,0 +1,86 @@
+package controller
+
+import (
+	"net/http"
+	"products-api/model"
+	"products-api/usecase"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type productController struct {
+	productUsecase usecase.ProductUsecase
+}
+
+func NewProductContrtoller(usecase usecase.ProductUsecase) productController {
+	return productController{
+		productUsecase: usecase,
+	}
+}
+
+func (p *productController) GetProducts(ctx *gin.Context) {
+
+	products, err := p.productUsecase.GetProducts()
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, err)
+	}
+
+	ctx.JSON(http.StatusOK, products)
+}
+
+func (p *productController) CreateProduct(ctx *gin.Context) {
+
+	var product model.Product
+	err := ctx.BindJSON(&product)
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, err)
+		return
+	}
+
+	insertedProduct, err := p.productUsecase.CreateProduct(product)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, err)
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, insertedProduct)
+}
+
+func (p *productController) GetProduct(ctx *gin.Context) {
+	id := ctx.Param("productId")
+	if id == "" {
+		response := model.Response{
+			Message: "Product ID can't be null.",
+		}
+		ctx.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	productId, err := strconv.Atoi(id)
+	if err != nil {
+		response := model.Response{
+			Message: "Product ID must be a number.",
+		}
+		ctx.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	product, err := p.productUsecase.GetProduct(productId)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, err)
+	}
+
+	if product == nil {
+		response := model.Response{
+			Message: "Product not found.",
+		}
+		ctx.JSON(http.StatusNotFound, response)
+		return
+	}
+
+	ctx.JSON(http.StatusOK, product)
+}
